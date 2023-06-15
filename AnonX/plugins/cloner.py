e912446b58f9132859dbb65895b7597a7f269acc
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2023-06-15 08:56:24 +0530

    Update cloner.py
    Signed-off-by: ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>

--- AnonX/plugins/cloner.py
+++ AnonX/plugins/cloner.py
@@ -7,60 +7,32 @@ from pyrogram.types import *
 import random
 from random import choice
 from config import API_ID, API_HASH , MUSIC_BOT_NAME
-from pyrogram import Client
+from AnonX import client as sys
 from AnonX import app
 import config
 from strings import get_command
 from ..logging import LOGGER
 from pyrogram.errors.exceptions.bad_request_400 import AccessTokenExpired, AccessTokenInvalid
 
-cloner = "6183502276:AAGZVPG5vPEb8RFFsW41g-SstFLgXY75SuU"
-##Copy from here 
-@app.on_message((filters.regex(r'\d[0-9]{8,10}:[0-9A-Za-z_-]{35}')) & filters.private)
-async def on_clone(self, message):
-    user_id = message.from_user.id 
-    user_name = message.from_user.first_name
-    bot_token = re.findall(r'\d[0-9]{8,10}:[0-9A-Za-z_-]{35}', message.text, re.IGNORECASE)
-    bot_token = bot_token[0] if bot_token else None
-    bot_id = re.findall(r'\d[0-9]{8,10}', message.text)
-
-    if not str(message.forward_from.id) != "93372553":
-        msg = await message.reply_text(f"🔑 <code>{bot_token}</code>\n\nCopying system...")
-        try:
-            ai = Client(
-                f"{bot_token}", API_ID, API_HASH,
-                bot_token = bot_token,
-                plugins={"root": "AnonX.plugins"},
-            )
-            await ai.start()
-            bot = await ai.get_me()
-            details = {
-                
-                'bot_token': bot_token,
-                
-            }
-            await message.edit_text(f"✅ The bot @{bot.username} is now working like Groups Guard.\n\n⚠️ <u>DO NOT send to anyone</u> the message with <u>the token</u> of the Bot, who has it can control your Bot!\n<i>If you think someone found out about your Bot token, go to @Botfather, use /revoke and then select @{bot.username}</i>")
-        except BaseException as e:
-            await message.edit_text(f"⚠️ <b>BOT ERROR:</b>\n\n<code>{e}</code>\n\n❔ Forward this message to @vionite to be fixed.")
 
 
 @app.on_message(filters.private & filters.command("clone"))
-async def clone(client, message: Message):
-    chat = message.chat.id
-    text = await message.reply("Usage:\n\n /clone token")
-    cmd = message.get_command
-    phone = message.get_command[1]
+async def clone(bot, msg: Message):
+    chat = msg.chat.id
+    text = await msg.reply("Usage:\n\n /clone token")
+    cmd = msg.get_command
+    phone = msg.get_command[1]
     try:
         await text.edit("Booting Your Client")
                    # change this Directry according to ur repo
         client = Client(":memory:", API_ID, API_HASH, bot_token=phone,plugins={"root": "AnonX.plugins"})
         await client.start()
         user = await client.get_me()
-        await message.reply(f"Your Client Has Been Successfully Started As @{user.username}! ✅ \n\n Now Add Your Bot And Assistant @PREMIUMMUSICPLAYER1 To Your Chat!\n\nThanks for Cloning.")
+        await msg.reply(f"Your Client Has Been Successfully Started As @{user.username}! ✅ \n\n Now Add Your Bot And Assistant @PREMIUMMUSICPLAYER1 To Your Chat!\n\nThanks for Cloning.")
         APP_USERNAME = user.username
         await sys.send_message(APP_USERNAME, "/start")
     except Exception as e:
-        await message.reply(f"**ERROR:** `{str(e)}`\nPress /start to Start again.")
+        await msg.reply(f"**ERROR:** `{str(e)}`\nPress /start to Start again.")
 #End
 ##This code fit with every pyrogram Codes just import then @Client Xyz!
 
